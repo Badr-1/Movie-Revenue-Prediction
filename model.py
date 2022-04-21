9a9b960f87fdc557b261413daa230a4758b2240f
badr <badrmohamedashraf@gmail.com>
2022-04-21 02:46:15 +0200

    updated first model, added the other

--- model.py
+++ model.py
@@ -18,7 +18,19 @@ import string
 API_KEY = "95a1eb7e26b4cc39736c3f8dd4d719b0"
 
 
-# data = pd.read_csv('merged_w_directors.csv')
+def get_movie(movie_name):
+    movie_name = movie_name.translate(str.maketrans('', '', string.punctuation)).replace(' ', '+')
+    response = requests.get(
+        'https://api.themoviedb.org/3/search/movie?api_key={0}&query={1}'.format(API_KEY, movie_name))
+    if (response.status_code == 200):
+        # returns id of the first result as it is most likely to be the movie we're looking for
+        if (len(response.json()["results"]) > 0):
+            return response.json()["results"][0]['id']
+        else:
+            print("Movie name: {0}".format(movie_name))
+    return
+
+
 def get_genre(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{0}?api_key={1}'.format(movie_id, API_KEY))
     if (response.status_code == 200):
@@ -52,19 +64,6 @@ def get_release_date(movie_id):
         date = response.json()['release_date']
         formatted_date = datetime.datetime.strptime(date, '%Y-%m-%d').strftime('%d-%b-%y')
         return formatted_date
-    return
-
-
-def get_movie(movie_name):
-    movie_name = movie_name.translate(str.maketrans('', '', string.punctuation)).replace(' ', '+')
-    response = requests.get(
-        'https://api.themoviedb.org/3/search/movie?api_key={0}&query={1}'.format(API_KEY, movie_name))
-    if (response.status_code == 200):
-        # returns id of the first result as it is most likely to be the movie we're looking for
-        if (len(response.json()["results"]) > 0):
-            return response.json()["results"][0]['id']
-        else:
-            print("Movie name: {0}".format(movie_name))
     return
 
 
@@ -181,15 +180,37 @@ def genre_one_hot_encoding():
     data.drop(['genre'], axis=1, inplace=True)
 
 
+def genre_label_encoding():
+    lbl_encode = LabelEncoder()
+    data['genre_label'] = lbl_encode.fit_transform(data['genre'])
+
+    data.drop(['genre'], axis=1, inplace=True)
+
+
+def genre_ordinal_encoding():
+    mapped_genre = {'Comedy': 9, 'Adventure': 13, 'Drama': 7, 'Action': 12, 'Musical': 15, 'Romantic Comedy': 8,
+                    'Horror': 3, \
+                    'Thriller/Suspense': 6, 'Crime': 1, 'Documentary': 2, 'Fantasy': 14, 'Black Comedy': 4,
+                    'Western': 5, \
+                    'Romance': 10, 'Animation': 11}
+    data['genre'] = data['genre'].replace(mapped_genre)
+
+
+def calculate_genre_weights():
+    genres = data['genre'].value_counts()
+    weights = data['genre'].value_counts()
+    for x in genres.index:
+        weights[x] = (data['revenue'].loc[data['genre'] == x].sum()) / genres[x]
+
 def release_date_feature_extraction():
     data = pd.read_csv('merged_full_datavTest.csv')
     data['day'] = pd.DatetimeIndex(data['release_date']).day
     data['month'] = pd.DatetimeIndex(data['release_date']).month
     data['weekend'] = 0
-    # data['Summer'] = 0
-    # data['Spring'] = 0
-    # data['Autumn'] = 0
-    # data['Winter'] = 0
+    data['Summer'] = 0
+    data['Spring'] = 0
+    data['Autumn'] = 0
+    data['Winter'] = 0
     data['new_movie'] = 0
     data['release_date'] = data['release_date'].str[-2:].astype(int)
     # data['revenue'] = data['revenue'].str[1:].replace(',', '', regex=True).astype('int64')
@@ -245,7 +266,7 @@ def merge_data():
     return merged_data
 
 
-def directors_encoding():
+def directors_modified_one_hot_encoding():
     directors_movies = data['director'].value_counts()
     for x in directors_movies.index:
         if directors_movies[x] >= 3:
@@ -254,8 +275,9 @@ def directors_encoding():
 
     data.drop(['director'], axis=1, inplace=True)
 
-# def director_target_encoding():
 
+def director_target_encoding():
+    data['director'] = data.groupby('director')["revenue"].transform("mean")
 
 
 def preprocessing():
@@ -276,7 +298,7 @@ def preprocessing():
 
 
 # ==============================================================
-
+#TODO
 # data = merge_data()
 # fill_revenue()
 # fill_release_date()
@@ -295,56 +317,53 @@ data.drop_duplicates(inplace=True)
 data.dropna(inplace=True, subset=['revenue', 'director', 'MPAA_rating'])
 
 data = data[data.revenue != 0]
+# ENCODING
 
 # rating_label_encoding()
 # rating_one_hot_encoding()
 rating_ordinal_encoding()
 
-genre_one_hot_encoding()
+calculate_genre_weights()
+genre_ordinal_encoding()
+# genre_label_encoding()
+# genre_one_hot_encoding()
+
 data.to_csv('merged_full_datavTest.csv', index=False)
 data = release_date_feature_extraction()
-directors_encoding()
+
+# directors_modified_one_hot_encoding()
+director_target_encoding()
 data = data[[col for col in data.columns if col != "revenue"] + ["revenue"]]
 
+# dividing data
 y = data['revenue']
-x = data.iloc[:,0:-2]
+x = data.iloc[:, 0:-2]
 
-
-# scaling
+# SCALING
 scaler = MinMaxScaler()
 x = scaler.fit_transform(x)
 
+# splitting data to train and test
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.20, shuffle=True, random_state=45)
 
+# RIDGE MODEL
+ridg = linear_model.Ridge()
+ridg.fit(x_train, y_train)
 
-cls = linear_model.LinearRegression()
-cls.fit(x_train, y_train)
-prediction_linear = cls.predict(x_test)
+prediction_ridge_train = ridg.predict(x_train)
+print('MSE ridge {Train}', metrics.mean_squared_error(np.asarray(y_train), prediction_ridge_train))
+prediction_ridge = ridg.predict(x_test)
+print('MSE ridge {Test}', metrics.mean_squared_error(np.asarray(y_test), prediction_ridge))
 
-print('Mean Square Error', metrics.mean_squared_error(np.asarray(y_test), prediction_linear))
-data.to_csv('merged_full_datavTest.csv', index=False)
+# Poly Model
+poly_features = PolynomialFeatures(degree=3)
+x_train_poly = poly_features.fit_transform(x_train)
+x_test_poly = poly_features.fit_transform(x_test)
 
+poly_model = linear_model.LinearRegression()
+poly_model.fit(x_train_poly, y_train)
 
-
-
-
-# poly_features = PolynomialFeatures(degree=3)
-# x_train_poly = poly_features.fit_transform(x_train)
-#
-# poly_model = linear_model.LinearRegression()
-# poly_model.fit(x_train_poly, y_train)
-#
-#
-# y_train_predicted = poly_model.predict(x_train_poly)
-# ypred = poly_model.predict(poly_features.transform(x_test))
-#
-# # predicting on test data-set
-# prediction_ploy = poly_model.predict(poly_features.fit_transform(x_test))
-#
-# print('Mean Square Error', metrics.mean_squared_error(y_test, prediction_ploy))
-#
-# true_film_revenue = np.asarray(y_test)[0]
-# predicted_film_revenue = prediction_ploy[0]
-# print('True revenue for the first film in the test set in millions is : ' + str(true_film_revenue))
-# print('Predicted revenue for the first film in the test set in millions is : ' + str(predicted_film_revenue))
-
+prediction_ploy_train = poly_model.predict(x_train_poly)
+print('MSE Poly {TRAIN}', metrics.mean_squared_error(y_train, prediction_ploy_train))
+prediction_ploy_test = poly_model.predict(x_test_poly)
+print('MSE Poly {TEST}', metrics.mean_squared_error(y_test, prediction_ploy_test))
